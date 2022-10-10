29e014546392cdffe4f8cb6cb373b84dfbb654bc
Owen Young <theowenyoung@gmail.com>
2022-10-10 22:42:05 +0800

    add time

---
+++ build-by-source.ts
@@ -0,0 +1,404 @@
+import {
+  CSS,
+  fromMarkdown,
+  gfm,
+  gfmFromMarkdown,
+  gfmToMarkdown,
+  groupBy,
+  jsonfeedToAtom,
+  mustache,
+  titleCase,
+  toMarkdown,
+} from "./deps.ts";
+import { DB, fs, path } from "./deps.ts";
+import Github from "./adapters/github.ts";
+import {
+  BuiltMarkdownInfo,
+  DayInfo,
+  DbMetaSource,
+  Feed,
+  FeedItem,
+  File,
+  FileInfo,
+  FileMeta,
+  FileMetaWithSource,
+  Item,
+  ItemDetail,
+  ItemsJson,
+  RunOptions,
+  Source,
+  WeekOfYear,
+} from "./interface.ts";
+import {
+  INDEX_HTML_PATH,
+  INDEX_MARKDOWN_PATH,
+  RECENTLY_UPDATED_COUNT,
+} from "./constant.ts";
+import {
+  exists,
+  formatHumanTime,
+  formatNumber,
+  getBaseFeed,
+  getDataItemsPath,
+  getDataRawPath,
+  getDayNumber,
+  getDbMeta,
+  getDistRepoGitUrl,
+  getDistRepoPath,
+  getDomain,
+  getItemsDetails,
+  getPublicPath,
+  getRepoHTMLURL,
+  getUTCDay,
+  getWeekNumber,
+  isDev,
+  isMock,
+  parseDayInfo,
+  parseItemsFilepath,
+  parseWeekInfo,
+  readJSONFile,
+  readTextFile,
+  sha1,
+  slugy,
+  startDateOfWeek,
+  walkFile,
+  writeDbMeta,
+  writeJSONFile,
+  writeTextFile,
+} from "./util.ts";
+import log from "./log.ts";
+import { getItems, getUpdatedFiles } from "./db.ts";
+import formatMarkdownItem from "./format-markdown-item.ts";
+import buildHtml from "./build-html.ts";
+import renderMarkdown from "./render-markdown.ts";
+let htmlIndexTemplateContent = "";
+export default async function main(
+  db: DB,
+  fileInfo: FileInfo,
+  runOptions: RunOptions,
+): Promise<BuiltMarkdownInfo> {
+  const config = runOptions.config;
+  const siteConfig = config.site;
+  const dbMeta = await getDbMeta();
+  const dbSources = dbMeta.sources;
+  const sourceConfig = fileInfo.sourceConfig;
+  const sourceCategory = sourceConfig.category;
+  const sourceMeta = fileInfo.sourceMeta;
+  const filepath = fileInfo.filepath;
+  const fileConfig = sourceConfig.files[filepath];
+  const repoMeta = sourceMeta.meta;
+  const sourceIdentifier = sourceConfig.identifier;
+  const dbSource = dbSources[sourceIdentifier];
+  const originalFilepath = fileConfig.filepath;
+  const commitMessage = `Update ${sourceIdentifier}/${originalFilepath}`;
+  const sourceFileConfig = fileConfig;
+  // get items
+  const items = getItems(db, sourceIdentifier, originalFilepath);
+  const dbFileMeta = dbSource.files[originalFilepath];
+  const distRepoPath = getDistRepoPath();
+  const domain = getDomain();
+  const isBuildMarkdown = runOptions.markdown;
+  const isBuildHtml = runOptions.html;
+  if (!isBuildMarkdown && !isBuildHtml) {
+    return {
+      commitMessage,
+    };
+  }
+  if (!htmlIndexTemplateContent) {
+    htmlIndexTemplateContent = await readTextFile("./templates/index.html.mu");
+  }
+  let relativeFolder = sourceIdentifier;
+  if (!sourceFileConfig.index) {
+    // to README.md path
+    const filepathExtname = path.extname(originalFilepath);
+    const originalFilepathWithoutExt = originalFilepath.slice(
+      0,
+      -filepathExtname.length,
+    );
+    relativeFolder = path.join(relativeFolder, originalFilepathWithoutExt);
+  }
+  for (let i = 0; i < 2; i++) {
+    const baseFeed = getBaseFeed();
+    const isDay = i === 0;
+    let currentNavHeader =
+      `[ Daily / [Weekly](/${sourceIdentifier}/week/${INDEX_MARKDOWN_PATH}) / [Overview](/${sourceIdentifier}/readme/${INDEX_MARKDOWN_PATH}) ]`;
+    if (!isDay) {
+      currentNavHeader =
+        `[ [Daily](/${sourceIdentifier}/${INDEX_MARKDOWN_PATH}) / Weekly / [Overview](/${sourceIdentifier}/readme/${INDEX_MARKDOWN_PATH}) ]`;
+    }
+    const nav = `[Home](/${INDEX_MARKDOWN_PATH}) · [Feed](/${relativeFolder}/${
+      isDay ? "" : "week/"
+    }feed.json) · [Repo](${
+      getRepoHTMLURL(repoMeta.url, repoMeta.default_branch, originalFilepath)
+    }) · ${sourceCategory} · ⭐ ${
+      formatNumber(repoMeta.stargazers_count)
+    } ·  📝 ${formatHumanTime(new Date(dbFileMeta.updated_at))} · ✅ ${
+      formatHumanTime(new Date(dbFileMeta.checked_at))
+    } 
+
+${currentNavHeader}
+`;
+    const feedTitle = `Track ${titleCase(repoMeta.name)}  ${
+      isDay ? "Daily" : "Weekly"
+    }`;
+    const feedDescription = repoMeta.description;
+    const footer = ``;
+    const allItems: ItemDetail[] = getItemsDetails(items);
+    const groups = groupBy(
+      allItems,
+      isDay ? "updated_day" : "updated_week",
+    ) as Record<
+      string,
+      ItemDetail[]
+    >;
+    const groupKeys = Object.keys(groups);
+    // sort
+    groupKeys.sort((a: string, b: string) => {
+      if (isDay) {
+        return parseDayInfo(Number(b)).date.getTime() -
+          parseDayInfo(Number(a)).date.getTime();
+      } else {
+        return parseWeekInfo(Number(b)).date.getTime() -
+          parseWeekInfo(Number(a)).date.getTime();
+      }
+    });
+    const dailyRelativeFolder = isDay
+      ? relativeFolder
+      : path.join(relativeFolder, `week`);
+
+    let feedItems: FeedItem[] = groupKeys.map((key) => {
+      const items = groups[key];
+      const categoryGroup = groupBy(items, "category") as Record<
+        string,
+        ItemDetail[]
+      >;
+      let groupMarkdown = "";
+      const categoryKeys: string[] = Object.keys(categoryGroup);
+      const today = new Date();
+      const tomorrow = new Date(today);
+      tomorrow.setDate(tomorrow.getDate() + 1);
+      let datePublished: Date = tomorrow;
+      let dateModified: Date = new Date(0);
+      categoryKeys.forEach((key: string) => {
+        groupMarkdown += `### ${key}\n\n`;
+        categoryGroup[key].forEach((item) => {
+          groupMarkdown += `${item.markdown}\n`;
+          const itemUpdatedAt = new Date(item.updated_at);
+          if (itemUpdatedAt.getTime() > dateModified.getTime()) {
+            dateModified = itemUpdatedAt;
+          }
+          if (itemUpdatedAt.getTime() < datePublished.getTime()) {
+            datePublished = itemUpdatedAt;
+          }
+        });
+      });
+      let dayInfo: DayInfo | WeekOfYear;
+      if (isDay) {
+        dayInfo = parseDayInfo(Number(key));
+      } else {
+        dayInfo = parseWeekInfo(Number(key));
+      }
+      const slug = isDay ? dayInfo.path + "/" : dayInfo.path + "/week/";
+      const itemUrl = `${domain}/${dayInfo.path}/#${slugy(fileConfig.name)}`;
+      const url = `${domain}/${slug}`;
+      const feedItem: FeedItem = {
+        id: itemUrl,
+        title: dayInfo.name,
+        _slug: slug,
+        url: itemUrl,
+        external_url: url,
+        _external_slug: slug,
+        date_published: datePublished.toISOString(),
+        date_modified: dateModified.toISOString(),
+        content_text: groupMarkdown,
+        content_html: renderMarkdown(groupMarkdown),
+      };
+      return feedItem;
+    });
+
+    // sort feedItems by date published
+    feedItems = feedItems.sort((a, b) => {
+      const aDate = new Date(a.date_published);
+      const bDate = new Date(b.date_published);
+      return bDate.getTime() - aDate.getTime();
+    });
+
+    const title = `Track ${repoMeta.name}  ${isDay ? "Daily" : "Weekly"}`;
+    const feed: Feed = {
+      ...baseFeed,
+      title,
+      _seo_title: `${title} - ${siteConfig.title}`,
+      description: repoMeta.description,
+      home_page_url: `${domain}/${dailyRelativeFolder}/`,
+      feed_url: `${domain}/${dailyRelativeFolder}/feed.json`,
+      items: feedItems,
+      _nav_text: nav,
+    };
+    const markdownDoc = `# ${feed.title}
+
+${feed.description}
+
+${feed._nav_text}
+
+${
+      feedItems.map((item) => {
+        return `## [${item.title}](/${item._external_slug}${INDEX_MARKDOWN_PATH})
+
+${item.content_text}
+`;
+      }).join("\n\n")
+    }
+
+`;
+    if (isBuildMarkdown) {
+      const markdownDistPath = path.join(
+        getDistRepoPath(),
+        dailyRelativeFolder,
+        INDEX_MARKDOWN_PATH,
+      );
+      await writeTextFile(markdownDistPath, markdownDoc);
+      log.debug(`build ${markdownDistPath} success`);
+    }
+    // build html
+    if (isBuildHtml) {
+      // add body, css to feed
+      const body = renderMarkdown(markdownDoc);
+      const htmlDoc = mustache.render(htmlIndexTemplateContent, {
+        ...feed,
+        body,
+        CSS,
+      });
+      const htmlDistPath = path.join(
+        getPublicPath(),
+        dailyRelativeFolder,
+        INDEX_HTML_PATH,
+      );
+      await writeTextFile(htmlDistPath, htmlDoc);
+      log.debug(`build ${htmlDistPath} success`);
+
+      // build feed json
+      const feedJsonDistPath = path.join(
+        getPublicPath(),
+        dailyRelativeFolder,
+        "feed.json",
+      );
+      // remote the current day feed, cause there is maybe some new items
+
+      if (isDay) {
+        // today start
+        const today = new Date();
+        const todayStart = new Date(
+          today.getUTCFullYear(),
+          today.getUTCMonth(),
+          today.getUTCDate(),
+        );
+        const todayStartTimestamp = todayStart.getTime();
+        feedItems = feedItems.filter((item) => {
+          const itemDate = new Date(item.date_published);
+          return itemDate.getTime() < todayStartTimestamp;
+        });
+      } else {
+        // week
+        // get week start date
+        const startWeekDate = startDateOfWeek(new Date());
+        const startWeekDateTimestamp = startWeekDate.getTime();
+        feedItems = feedItems.filter((item) => {
+          const itemDate = new Date(item.date_published);
+          return itemDate.getTime() < startWeekDateTimestamp;
+        });
+      }
+      feed.items = feedItems;
+
+      await writeJSONFile(feedJsonDistPath, feed);
+      // build rss
+      // @ts-ignore: node modules
+      const feedOutput = jsonfeedToAtom(feed, {
+        language: "en",
+      });
+      const rssDistPath = path.join(
+        getPublicPath(),
+        dailyRelativeFolder,
+        "feed.xml",
+      );
+      await writeTextFile(rssDistPath, feedOutput);
+    }
+  }
+
+  // build overview markdown
+  // first get readme content
+
+  const api = new Github(sourceConfig);
+  const readmeContent = await api.getConent(filepath);
+
+  const currentNavHeader =
+    `[ [Daily](/${relativeFolder}/${INDEX_MARKDOWN_PATH}) / [Weekly](/${relativeFolder}/week/${INDEX_MARKDOWN_PATH}) / Overview ]`;
+
+  const nav = `
+[Home](/${INDEX_MARKDOWN_PATH}) · [Feed](/${relativeFolder}/feed.json) · [Repo](${
+    getRepoHTMLURL(repoMeta.url, repoMeta.default_branch, filepath)
+  }) · ⭐ ${formatNumber(repoMeta.stargazers_count)} ·  📝 ${
+    formatHumanTime(new Date(dbFileMeta.updated_at))
+  } · ✅ ${formatHumanTime(new Date(dbFileMeta.checked_at))} 
+
+${currentNavHeader}
+
+---
+`;
+  const overviewMarkdownPath = path.join(
+    getDistRepoPath(),
+    relativeFolder,
+    "readme",
+    INDEX_MARKDOWN_PATH,
+  );
+  const overviewHtmlPath = path.join(
+    getPublicPath(),
+    relativeFolder,
+    "readme",
+    INDEX_HTML_PATH,
+  );
+  const tree = fromMarkdown(readmeContent, "utf8", {
+    extensions: [gfm()],
+    mdastExtensions: [gfmFromMarkdown()],
+  });
+
+  // format link etc.
+  const overviewMarkdownTree = await formatMarkdownItem(tree, fileInfo);
+  const overviewMarkdownContent = toMarkdown(
+    overviewMarkdownTree,
+    {
+      extensions: [gfmToMarkdown()],
+    },
+  );
+
+  const readmeRendered = `# ${repoMeta.name}
+
+${repoMeta.description}
+
+${nav}
+
+${overviewMarkdownContent}
+`;
+  await writeTextFile(overviewMarkdownPath, readmeRendered);
+  log.debug(`build ${overviewMarkdownPath} success`);
+  if (isBuildHtml) {
+    // add body, css to feed
+    const body = renderMarkdown(readmeRendered);
+    const htmlDoc = mustache.render(htmlIndexTemplateContent, {
+      _seo_title: `${titleCase(repoMeta.name)}`,
+      body: body,
+      CSS,
+    });
+    const htmlDistPath = path.join(
+      getPublicPath(),
+      relativeFolder,
+      "readme",
+      INDEX_HTML_PATH,
+    );
+    await writeTextFile(htmlDistPath, htmlDoc);
+    log.debug(`build ${htmlDistPath} success`);
+  }
+
+  return {
+    commitMessage,
+  };
+}
---
+++ build-by-time.ts
@@ -0,0 +1,368 @@
+import { CSS, groupBy, mustache } from "./deps.ts";
+import { DB, fs, path } from "./deps.ts";
+import {
+  BuiltMarkdownInfo,
+  Config,
+  DbMetaSource,
+  Feed,
+  FeedItem,
+  File,
+  FileMeta,
+  FileMetaWithSource,
+  Item,
+  ItemDetail,
+  ItemsJson,
+  RunOptions,
+  Source,
+} from "./interface.ts";
+import {
+  INDEX_HTML_PATH,
+  INDEX_MARKDOWN_PATH,
+  RECENTLY_UPDATED_COUNT,
+} from "./constant.ts";
+import {
+  exists,
+  getBaseFeed,
+  getDataItemsPath,
+  getDataRawPath,
+  getDbMeta,
+  getDistRepoGitUrl,
+  getDistRepoPath,
+  getDomain,
+  getItemsDetails,
+  getPublicPath,
+  getUTCDay,
+  isDev,
+  parseDayInfo,
+  parseItemsFilepath,
+  parseWeekInfo,
+  readJSONFile,
+  readTextFile,
+  sha1,
+  walkFile,
+  writeDbMeta,
+  writeJSONFile,
+  writeTextFile,
+} from "./util.ts";
+import renderMarkdown from "./render-markdown.ts";
+import log from "./log.ts";
+import { getDayItems, getItems, getUpdatedFiles, getWeekItems } from "./db.ts";
+let htmlIndexTemplateContent = "";
+function groupByFile(item: Item) {
+  return item.source_identifier + "/" + item.file;
+}
+export default async function main(
+  db: DB,
+  number: number,
+  options: RunOptions,
+): Promise<BuiltMarkdownInfo> {
+  // test is day or week
+  const domain = getDomain();
+  const isDay = number.toString().length === 8;
+  const isBuildMarkdown = options.markdown || false;
+  const isBuildSite = options.html || false;
+
+  if (!isBuildMarkdown && !isBuildSite) {
+    log.info("skip build timeline markdown and html");
+    return {
+      commitMessage: "",
+    };
+  }
+
+  const config = options.config;
+  const sourcesConfig = config.sources;
+  let title = "";
+  let commitMessage = "";
+  let items: Record<string, Item> = {};
+  let distMarkdownRelativePath = "";
+  const baseFeed = getBaseFeed();
+  let feedTitle = "";
+  let feedDescription = "";
+  if (!htmlIndexTemplateContent) {
+    htmlIndexTemplateContent = await readTextFile("./templates/index.html.mu");
+  }
+  if (isDay) {
+    const dayInfo = parseDayInfo(number);
+    commitMessage = `Update day ${dayInfo.path}`;
+    title = dayInfo.name;
+    distMarkdownRelativePath = dayInfo.path;
+    // get items
+    items = getDayItems(db, number);
+  } else {
+    const weekInfo = parseWeekInfo(number);
+    commitMessage = `Update week ${weekInfo.path}`;
+    title = weekInfo.name;
+    distMarkdownRelativePath = weekInfo.path;
+    // get items
+    items = getWeekItems(db, number);
+  }
+  feedTitle = `${title}`;
+  feedDescription = `Awesome list updated on ${title}`;
+
+  const feedItems = itemsToFeedItems(items, config);
+  const nav = `[Home](/) | [Week](/week/)`;
+  const feed: Feed = {
+    ...baseFeed,
+    title: feedTitle,
+    description: feedDescription,
+    _seo_title: feedTitle,
+    feed_url: `${domain}/feed.json`,
+    home_page_url: domain,
+    _nav_text: nav,
+    items: feedItems,
+  };
+
+  const markdownDoc = `# ${feed.title}
+
+${feed.description}
+
+${feed._nav_text}
+
+${
+    feedItems.map((item) => {
+      return `## [${item.title}](/${item._slug}${INDEX_MARKDOWN_PATH})
+
+${item.content_text}
+`;
+    }).join("\n\n")
+  }
+
+`;
+  if (isBuildMarkdown) {
+    // build daily markdown
+    // sort
+    const distRepoPath = getDistRepoPath();
+    const dailyMarkdownPath = path.join(
+      distRepoPath,
+      distMarkdownRelativePath,
+      INDEX_MARKDOWN_PATH,
+    );
+
+    await writeTextFile(dailyMarkdownPath, markdownDoc);
+    // log.debug(`build ${dailyMarkdownPath} success`);
+  }
+  if (isBuildSite) {
+    // add body, css to feed
+    const body = renderMarkdown(markdownDoc);
+    const htmlDoc = mustache.render(htmlIndexTemplateContent, {
+      ...feed,
+      body,
+      CSS,
+    });
+    const htmlDistPath = path.join(
+      getPublicPath(),
+      distMarkdownRelativePath,
+      INDEX_HTML_PATH,
+    );
+    await writeTextFile(htmlDistPath, htmlDoc);
+    log.debug(`build ${htmlDistPath} success`);
+
+    // build feed json
+    const feedJsonDistPath = path.join(
+      getPublicPath(),
+      distMarkdownRelativePath,
+      "feed.json",
+    );
+    await writeJSONFile(feedJsonDistPath, feed);
+  }
+  return {
+    commitMessage,
+  };
+}
+
+export function itemsToFeedItems(
+  items: Record<string, Item>,
+  config: Config,
+): FeedItem[] {
+  const allItems: Item[] = [];
+  for (const itemSha1 of Object.keys(items)) {
+    const item = items[itemSha1];
+    allItems.push(item);
+  }
+  const domain = getDomain();
+  const sourcesConfig = config.sources;
+  const groups = groupBy(allItems, groupByFile) as Record<
+    string,
+    Item[]
+  >;
+  const groupKeys = Object.keys(groups);
+  let feedItems: FeedItem[] = groupKeys.map((key) => {
+    const items = groups[key];
+
+    let groupMarkdown = "";
+    const categoryGroup = groupBy(items, "category") as Record<
+      string,
+      Item[]
+    >;
+
+    const categoryKeys = Object.keys(categoryGroup);
+    if (categoryKeys.length === 0) {
+      throw new Error(`${key} has no categories`);
+    }
+    let firstItem: Item | undefined;
+
+    const today = new Date();
+    const tomorrow = new Date(today);
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    let datePublished: Date = tomorrow;
+    let dateModified: Date = new Date(0);
+    categoryKeys.forEach((key) => {
+      groupMarkdown += `### ${key}\n\n`;
+      categoryGroup[key].forEach((item) => {
+        groupMarkdown += item.markdown + "\n";
+        firstItem = item;
+        const itemUpdated = new Date(item.updated_at);
+        if (itemUpdated.getTime() < datePublished.getTime()) {
+          datePublished = itemUpdated;
+        }
+        if (itemUpdated.getTime() > dateModified.getTime()) {
+          dateModified = itemUpdated;
+        }
+      });
+    });
+    if (!firstItem) {
+      throw new Error(`${key} has no firstItem`);
+    }
+    // get file path
+    const sourceFileConfig =
+      sourcesConfig[firstItem.source_identifier].files[firstItem.file];
+
+    const slug = sourceFileConfig.pathname.slice(1);
+
+    const itemUrl = `${domain}/${slug}`;
+    const feedItem: FeedItem = {
+      id: itemUrl,
+      title: sourceFileConfig.name,
+      _slug: slug,
+      url: itemUrl,
+      content_text: groupMarkdown,
+      content_html: renderMarkdown(groupMarkdown),
+      date_published: datePublished.toISOString(),
+      date_modified: dateModified.toISOString(),
+    };
+    return feedItem;
+  });
+  // sort feedItems by date published
+  feedItems = feedItems.sort((a, b) => {
+    const aDate = new Date(a.date_published);
+    const bDate = new Date(b.date_published);
+    return bDate.getTime() - aDate.getTime();
+  });
+  return feedItems;
+}
+export function itemsToFeedItemsByDate(
+  items: Record<string, Item>,
+  config: Config,
+  isDay: boolean,
+): FeedItem[] {
+  const allItems: ItemDetail[] = getItemsDetails(items);
+  const domain = getDomain();
+  const sourcesConfig = config.sources;
+  const groups = groupBy(
+    allItems,
+    isDay ? "updated_day" : "updated_week",
+  ) as Record<
+    string,
+    ItemDetail[]
+  >;
+  let groupKeys = Object.keys(groups);
+  // sort
+  groupKeys = groupKeys.sort((a: string, b: string) => {
+    if (isDay) {
+      return parseDayInfo(Number(b)).date.getTime() -
+        parseDayInfo(Number(a)).date.getTime();
+    } else {
+      return parseWeekInfo(Number(b)).date.getTime() -
+        parseWeekInfo(Number(a)).date.getTime();
+    }
+  });
+  // removve the last one
+  groupKeys = groupKeys.slice(0, groupKeys.length - 1);
+  let feedItems: FeedItem[] = groupKeys.map((key) => {
+    const items = groups[key];
+
+    let groupMarkdown = "";
+    const categoryGroup = groupBy(items, groupByFile) as Record<
+      string,
+      ItemDetail[]
+    >;
+
+    const categoryKeys = Object.keys(categoryGroup);
+    if (categoryKeys.length === 0) {
+      throw new Error(`${key} has no categories`);
+    }
+    let firstItem: Item | undefined;
+
+    const today = new Date();
+    const tomorrow = new Date(today);
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    let datePublished: Date = tomorrow;
+    let dateModified: Date = new Date(0);
+    categoryKeys.forEach((key) => {
+      const firstSourceItem = categoryGroup[key][0];
+      const sourceFileConfig = sourcesConfig[firstSourceItem.source_identifier]
+        .files[firstSourceItem.file];
+      groupMarkdown +=
+        `#### [${sourceFileConfig.name}](${sourceFileConfig.pathname}${INDEX_MARKDOWN_PATH})\n\n`;
+      // group by category
+      const categoryItems = categoryGroup[key];
+      const categoryGroupByCategory = groupBy(
+        categoryItems,
+        "category",
+      );
+
+      const categoryGroupByCategoryKeys = Object.keys(
+        categoryGroupByCategory,
+      );
+      categoryGroupByCategoryKeys.forEach((categoryKey) => {
+        const items = categoryGroupByCategory[categoryKey];
+        groupMarkdown += `##### ${categoryKey}\n\n`;
+
+        items.forEach((item: ItemDetail) => {
+          groupMarkdown += item.markdown + "\n";
+          firstItem = item;
+          const itemUpdated = new Date(item.updated_at);
+          if (itemUpdated.getTime() < datePublished.getTime()) {
+            datePublished = itemUpdated;
+          }
+          if (itemUpdated.getTime() > dateModified.getTime()) {
+            dateModified = itemUpdated;
+          }
+        });
+        groupMarkdown += "\n\n";
+      });
+    });
+    if (!firstItem) {
+      throw new Error(`${key} has no firstItem`);
+    }
+    // get file path
+    const sourceFileConfig =
+      sourcesConfig[firstItem.source_identifier].files[firstItem.file];
+
+    const dayInfo = isDay
+      ? parseDayInfo(Number(key))
+      : parseWeekInfo(Number(key));
+    const slug = dayInfo.path + "/";
+    const itemUrl = `${domain}/${slug}`;
+    const feedItem: FeedItem = {
+      id: itemUrl,
+      title: dayInfo.name,
+      _title_id: dayInfo.id,
+      _title_suffix: "",
+      _slug: slug,
+      url: itemUrl,
+      content_text: groupMarkdown,
+      content_html: renderMarkdown(groupMarkdown),
+      date_published: datePublished.toISOString(),
+      date_modified: dateModified.toISOString(),
+    };
+    return feedItem;
+  });
+  // sort feedItems by date published
+  feedItems = feedItems.sort((a, b) => {
+    const aDate = new Date(a.date_published);
+    const bDate = new Date(b.date_published);
+    return bDate.getTime() - aDate.getTime();
+  });
+  return feedItems;
+}
