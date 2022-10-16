33e92d7cdd2893c46e75f2a5c4936c0d491aa8ad
Owen Young <theowenyoung@gmail.com>
2022-10-16 22:22:16 +0800

    test limit

--- build.ts
+++ build.ts
@@ -180,7 +180,7 @@ export default async function buildMarkdown(options: RunOptions) {
     );
     let updatedFileIndex = 0;
     let promises: Promise<void>[] = [];
-    let limit = pLimit(10);
+    let limit = pLimit(100);
     for (const file of allUpdatedFiles) {
       const sourceConfig = sourcesConfig[file.source_identifier];
       const fileInfo: FileInfo = {
